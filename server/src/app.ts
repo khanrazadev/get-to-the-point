e84658be6e40b7917468bdfa4b3eddc89a25493c
Raza Khan <razakhanha05@gmail.com>
2026-09-23 18:24:12 +0530

    chore: configure production CORS

--- server/src/app.ts
+++ server/src/app.ts
@@ -10,7 +10,12 @@ import { authMiddleware } from "./middleware/auth.middleware.js";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+  }),
+);
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
